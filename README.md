d4280a1cd81ff4d1c9675b581c2d1d7e596ccfe8
rlarkin01 <rlarkin@veracode.com>
2020-05-27 15:33:33 -0400

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+    # Veracode Usage Script
+
+A simple example of usage of the Veracode API signing library provided on the [Veracode Help Center](https://help.veracode.com/reader/LMv_dtSHyb7iIxAQznC~9w/cCoBmgWWxUM4hOY54dTqgA) to return account data related to license use and user roles.
+
+## Setup
+
+Save Veracode API credentials in your home folder `~/.veracode/credentials`
+An example of how to create the Credentials file is also provided on the [Veracode Help Center](https://help.veracode.com/reader/LMv_dtSHyb7iIxAQznC~9w/zm4hbaPkrXi02YmacwH3wQ)
+
+Add multiple crednentials, one for each account to be queried.
+The bracketed name will become the account name. The credentials must map to either a human user with Admin and Security Lead roles or an API User with AdminAPI and ResultsAPI roles for each agency you wish to check entitlements for. The script will skip the [default] credentials. Example for retieving data for two agencies:
+
+    [default]
+    veracode_api_key_id = <YOUR_API_KEY_ID>
+    veracode_api_key_secret = <YOUR_API_KEY_SECRET>
+
+    [Account1]
+    veracode_api_key_id = <API_KEY_ID_FOR_Agency1>
+    veracode_api_key_secret = <API_KEY_SECRET_FOR_Agency1>
+
+    [Account2]
+    veracode_api_key_id = <API_KEY_ID_FOR_Agency2>
+    veracode_api_key_secret = <API_KEY_SECRET_FOR_Agency2>
+
+Install dependencies:
+
+    cd veracode-usage-script
+    pip install -r requirements.txt
+
+
+## Run
+
+Run by calling in a Terminal or cmd window:
+
+    python agency-usage.py
+    
+Results will be printed onscreen and saved to a csv file in the present working directory.
+
---
+++ agency-usage.py
@@ -0,0 +1,107 @@
+import sys
+import os
+from os.path import expanduser
+import re
+import requests
+import json
+import csv
+from datetime import datetime
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+
+
+admin_base = "https://api.veracode.com/api/authn/v2"
+appsec_base = "https://api.veracode.com/appsec/v1"
+headers = {"User-Agent": "Python HMAC Example"}
+
+
+def main():
+    creds = get_credentials()
+
+    filename = 'agencyDetails_'+datetime.now().strftime("%m.%d.%Y_%H:%M:%S")+'.csv'
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Agency_name","App_total","Static_SCA","Dynamic","Users","Greenlight","eLearning","Admin_count","Admin_usernames","Creator_count","Creator_usernames"])
+
+        for agency in creds:
+            if agency[0] != 'default':
+                os.environ["VERACODE_API_PROFILE"] = agency[0]
+                app_counts = get_app_counts()
+                user_counts = get_user_counts()
+                writer.writerow([agency[0],app_counts[0],app_counts[1],app_counts[2],user_counts[0],user_counts[1],user_counts[2],user_counts[3],user_counts[4],user_counts[5],user_counts[6]])
+
+    with open(filename, 'r') as f:
+        print(f.read())
+
+
+def get_credentials():
+    pattern = re.compile("\[(.*)\]\s*\nveracode_api_key_id = (.*)\s*\nveracode_api_key_secret = (.*)\s*\n")
+
+    f = open(expanduser("~") + "/.veracode/credentials", "r")
+    filetext = f.read()
+    f.close()
+
+    return re.findall(pattern, filetext)
+
+
+def get_app_counts():
+    response = make_api_call(appsec_base + "/applications")
+
+    if response.ok:
+        data = response.json()
+        total, static_sca, dynamic = 0, 0, 0
+        for app in data["_embedded"]["applications"]:
+            total+=1
+            for scan in app["scans"]:
+                if scan["scan_type"] == "STATIC":
+                    static_sca+=1
+                elif scan["scan_type"] == "DYNAMIC":
+                    dynamic+=1
+
+        return (total, static_sca, dynamic)
+    else:
+        print(response.status_code)
+
+
+def get_user_counts():
+    response = make_api_call(admin_base + "/users")
+
+    if response.ok:
+        data = response.json()
+        total, greenlight, elearn, admins, creators = 0,0,0,0,0
+        admin_names, creator_names = [], []
+        for user in data["_embedded"]["users"]:
+            total+=1
+            details = make_api_call(user["_links"]["self"]["href"])
+            if details.ok:
+                user = details.json()
+                creatorAlreadyAdded = False
+                for role in user["roles"]:
+                    description = role["role_description"]
+                    if description == "Administrator":
+                        admins+=1
+                        admin_names.append(user["user_name"])
+                    elif not creatorAlreadyAdded and (description == "Security Lead" or description == "Creator"):
+                        creators+=1
+                        creator_names.append(user["user_name"])
+                        creatorAlreadyAdded = True
+                    elif description == "Greenlight IDE User":
+                        greenlight+=1
+                    elif description == "eLearning":
+                        elearn+=1
+
+        return (total, greenlight, elearn, admins, ' AND '.join(admin_names), creators, ' AND '.join(creator_names))
+    else:
+        print(response.status_code)
+
+
+def make_api_call(url):
+    try:
+        return requests.get(url, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+    except requests.RequestException as e:
+        print("Whoops!")
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
